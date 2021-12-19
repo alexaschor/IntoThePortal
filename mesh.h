6d93bf91148e3ca4cce78fa068b9642a571538c7
Alexa Schor <alexaschor@gmail.com>
2021-12-19 18:37:58 -0500

    more flexible volumetric grid data structures

--- mesh.h
+++ mesh.h
@@ -1,15 +1,54 @@
 #ifndef MESH_H
 #define MESH_H
 #include <fstream>
+#include <iostream>
+#include <cstdio>
 #include <string>
 
 #include "SETTINGS.h"
+
+using namespace std;
 
 class Mesh {
 public:
     std::vector<VEC3F> vertices;
     std::vector<VEC3F> normals;
     std::vector<uint> indices;
+
+    //readOBJ function adapted from http://www.opengl-tutorial.org
+    void readOBJ(std::string filename) {
+        FILE * file = fopen(filename.c_str(), "r");
+        if (file == NULL){
+            printf("Could not open OBJ file %s for reading.\n", filename.c_str());
+            exit(1);
+        }
+        while (true){
+            char lineHeader[128];
+            // read the first word of the line
+            int res = fscanf(file, "%s", lineHeader);
+            if (res == EOF)
+                break;
+
+            if (strcmp(lineHeader, "v") == 0) {
+                VEC3F vertex;
+                fscanf(file, "%lf %lf %lf\n", &vertex.x(), &vertex.y(), &vertex.z());
+                vertices.push_back(vertex);
+            } else if (strcmp(lineHeader, "f") == 0){
+                size_t a_i, b_i, c_i;
+                int matches = fscanf(file, "%zd %zd %zd\n", &a_i, &b_i, &c_i);
+                if (matches != 3) {
+                    printf("Encountered malformed face data when reading OBJ %s. Make sure that UV and vertex normals are not included in the file.\n", filename.c_str());
+                    exit(1);
+                }
+
+                indices.push_back(a_i-1);
+                indices.push_back(b_i-1);
+                indices.push_back(c_i-1);
+            }
+        }
+
+        printf("Read %lu vertices and %lu faces from %s\n", vertices.size(), indices.size() / 3, filename.c_str());
+    }
 
     void writeOBJ(std::string filename) {
         std::ofstream out;
@@ -19,10 +58,9 @@ public:
         out << "g " << "Obj" << std::endl;
         for (size_t i = 0; i < vertices.size(); i++)
             out << "v " << vertices.at(i).x() << " " << vertices.at(i).y() << " " << vertices.at(i).z() << '\n';
-        for (size_t i = 0; i < vertices.size(); i++)
+        for (size_t i = 0; i < normals.size(); i++)
             out << "vn " << normals.at(i).x() << " " << normals.at(i).y() << " " << normals.at(i).z() << '\n';
-        for (size_t i = 0; i < indices.size(); i += 3)
-        {
+        for (size_t i = 0; i < indices.size(); i += 3) {
             out << "f " << indices.at(i) + 1 << "//" << indices.at(i) + 1
                 << " " << indices.at(i + 1) + 1 << "//" << indices.at(i + 1) + 1
                 << " " << indices.at(i + 2) + 1 << "//" << indices.at(i + 2) + 1
@@ -30,7 +68,9 @@ public:
         }
         out.close();
 
+        std::cout << "Wrote " << vertices.size() << " vertices and " << indices.size() / 3 << " faces to " << filename << std::endl;
     }
+
 };
 
 #endif
