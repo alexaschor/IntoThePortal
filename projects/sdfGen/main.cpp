c6312e0b173be6c40631c5f097ef4a5de3c2d1ea
Alexa Schor <alexaschor@gmail.com>
2022-03-11 10:30:22 -0500

    add mesh sequence sdf bounds checking

--- projects/sdfGen/main.cpp
+++ projects/sdfGen/main.cpp
@@ -14,11 +14,76 @@
 
 using namespace std;
 
+
 int main(int argc, char* argv[]) {
 
-    if(argc != 4) {
-        cout << "USAGE: " << argv[0] << " <*.obj input> <resolution> <*.f3d output>\n";
+    if(argc < 2) {
+        cout << "USAGE: " << endl;
+        cout << "To generate an SDF from a mesh with automatically generated bounds:" << endl;
+        cout << " " << argv[0] << " <*.obj input> <resolution> <*.f3d output>\n";
+        cout << "To get the bounds for a mesh sequence:" << endl;
+        cout << " " << argv[0] << " BOUNDS <obj 1> <obj 2> ... <obj N>\n";
+        cout << "To generate an SDF from a mesh with specified bounds:" << endl;
+        cout << " " << argv[0] << " <*.obj input> <resolution> <*.f3d output> <min X> <min Y> <min Z> <max X> <max Y> <max Z>\n";
         exit(-1);
+    }
+
+    if (string(argv[1]) == "BOUNDS") {
+        //start with a massive inside out bound box.
+        Vec3f min_box(numeric_limits<float>::max(),numeric_limits<float>::max(),numeric_limits<float>::max()),
+              max_box(-numeric_limits<float>::max(),-numeric_limits<float>::max(),-numeric_limits<float>::max());
+
+        for (int i = 2; i < argc; ++i) {
+            string filename(argv[i]);
+
+            if(filename.size() < 5 || filename.substr(filename.size()-4) != string(".obj")) {
+                cerr << "Error: Expected OBJ file with filename of the form <name>.obj.\n";
+                exit(-1);
+            }
+
+            cout << "Reading data.\n";
+
+            ifstream infile(argv[i]);
+            if(!infile) {
+                cerr << "Failed to open " << argv[i] << " Terminating.\n";
+                exit(-1);
+            }
+
+            int ignored_lines = 0;
+            int total_lines = 0;
+            string line;
+            while(!infile.eof()) {
+                getline(infile, line);
+
+                //.obj files sometimes contain vertex normals indicated by "vn"
+                if(line.substr(0,1) == string("v") && line.substr(0,2) != string("vn")){
+                    stringstream data(line);
+                    char c;
+                    Vec3f point;
+                    data >> c >> point[0] >> point[1] >> point[2];
+                    update_minmax(point, min_box, max_box);
+                }
+                else if( line.substr(0,2) == string("vn") ){
+                    cerr << "Obj-loader is not able to parse vertex normals, please strip them from the input file. \n";
+                    exit(-2);
+                }
+                else if(line.substr(0,1) == string("f")) { }
+                else {
+                    ++ignored_lines;
+                }
+                ++total_lines;
+            }
+            infile.close();
+
+            if(ignored_lines > 0)
+                cout << "Warning: " << ignored_lines << " of " << total_lines << " lines were ignored since they did not contain faces or vertices.\n";
+
+        }
+
+        PRINTV3(min_box);
+        PRINTV3(max_box);
+
+        exit(0);
     }
 
     string filename(argv[1]);
@@ -79,6 +144,35 @@ int main(int argc, char* argv[]) {
         cout << "Warning: " << ignored_lines << " lines were ignored since they did not contain faces or vertices.\n";
 
     cout << "Read in " << vertList.size() << " vertices and " << faceList.size() << " faces." << endl;
+
+
+    PRINT("Bounds from mesh:");
+    PRINTV3(min_box);
+    PRINTV3(max_box);
+
+    if (argc >= 10) {
+        float minX = atof(argv[4]);
+        float minY = atof(argv[5]);
+        float minZ = atof(argv[6]);
+
+        float maxX = atof(argv[7]);
+        float maxY = atof(argv[8]);
+        float maxZ = atof(argv[9]);
+
+        if ((minX > min_box[0]) || (minY > min_box[1]) || (minZ > min_box[2]) ||
+            (maxX < max_box[0]) || (maxY < max_box[1]) || (maxZ < max_box[1]))
+        {
+            PRINT("WARNING: User-specified bounds will truncate mesh!");
+        }
+
+        min_box = Vec3f(minX, minY, minZ);
+        max_box = Vec3f(maxX, maxY, maxZ);
+
+        PRINT("NOTE: Overriding with user-specified bounds");
+        PRINTV3(min_box);
+        PRINTV3(max_box);
+    }
+
 
     // find the center
     Vec3f center = (min_box + max_box) * 0.5;
