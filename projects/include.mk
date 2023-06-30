4f3c5988fb132d2bf6d675afbcd5d7e05957f09d
Alexa Schor <alexaschor@gmail.com>
2023-06-30 16:52:35 -0500

    update include.mk to remove unnecessary defines

--- projects/include.mk
+++ projects/include.mk
@@ -3,12 +3,10 @@
 # For OSX:
 CXX=g++-13
 CXXFLAGS=-Wall -MMD -g -O3 -std=c++11 -fopenmp -I../../lib -I../../src/ -I../../
-ANT_MAKEFILE_NAME=Makefile.osx
 LD_PATH_VAR=DYLD_LIBRARY_PATH
 
 
 # For Linux:
 # CXX=g++
-# CXXFLAGS=-Wall -MMD -g -std=c++11 -fopenmp -lGL -lGLU -lglut -I../../lib -I../../src/ -I../../
-# ANT_MAKEFILE_NAME=Makefile
+# CXXFLAGS=-Wall -MMD -g -std=c++11 -fopenmp -I../../lib -I../../src/ -I../../
 # LD_PATH_VAR=LD_LIBRARY_PATH
